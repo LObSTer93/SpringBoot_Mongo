9939ef4009b0dfd1876a707fa08b48c8b3325471
Nikita Katyshev <Nikita.Katyshev@sidenis.com>
2018-06-11 15:49:52 +0300

    springfox

--- src/main/java/com/lobster93/spring_boot_demo/config/SpringBootDemoApplication.java
+++ src/main/java/com/lobster93/spring_boot_demo/config/SpringBootDemoApplication.java
@@ -3,12 +3,21 @@ package com.lobster93.spring_boot_demo.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @ComponentScan({"com.lobster93.spring_boot_demo.service", "com.lobster93.spring_boot_demo.controller"})
 @EnableMongoRepositories("com.lobster93.spring_boot_demo.dao")
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
     @Override
@@ -16,15 +25,22 @@ public class SpringBootDemoApplication extends SpringBootServletInitializer {
         return application.sources(SpringBootDemoApplication.class);
     }
 
-//    @Autowired
-//    private CustomerService customerService;
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(SpringBootDemoApplication.class, args);
-//	}
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        customerService.save(new Customer());
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
 //    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.lobster93.spring_boot_demo.controller"))
+                .paths(regex("/customer.*"))
+                .build();
+
+    }
 }
--- src/main/java/com/lobster93/spring_boot_demo/controller/CustomerController.java
+++ src/main/java/com/lobster93/spring_boot_demo/controller/CustomerController.java
@@ -1,16 +1,17 @@
 package com.lobster93.spring_boot_demo.controller;
 
-import com.lobster93.spring_boot_demo.dao.Customer;
 import com.lobster93.spring_boot_demo.dto.CustomerDTO;
 import com.lobster93.spring_boot_demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
     @Autowired
